468e8f3e7cc995d92c6f8fa92be36de27654312a
David Ritchey <ritchey0713@gmail.com>
2019-04-12 22:13:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,6 +2,8 @@ class Artist
   
 attr_accessor :name, :songs
 
+@@all = []
+
 def initialize(name )
   @name = name 
   @songs = [] 
@@ -10,5 +12,13 @@ end
 def add_song(song)
   songs << song 
 end 
+
+def self.all
+  @@all 
+end 
+
+def save 
+  all << self 
+end
   
 end 
