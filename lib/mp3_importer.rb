2fc12377bb7ae1cca1f1df0cf22348f573db7b0d
David Ritchey <ritchey0713@gmail.com>
2019-04-12 21:29:47 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,20 @@
+require 'pry'
+class MP3Importer 
+  attr_accessor :path 
+  
+  def initialize(path)
+    @path = path 
+  end 
+  
+  def files
+    Dir.entries("#{path}").find_all {|file| file.include?("mp3")}
+  end 
+  
+  def import 
+
+    files.each do |file|
+      Song.new_by_filename(file)
+    end 
+  end 
+  
+end 
