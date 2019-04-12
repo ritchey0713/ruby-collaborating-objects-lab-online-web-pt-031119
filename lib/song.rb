b097c66496474324cf9f70b78e51eba56cb72673
David Ritchey <ritchey0713@gmail.com>
2019-04-12 22:31:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ class Song
   end 
   
   def self.new_by_filename(file) 
-    song, artist = file.split(" - ")
+    artist, song = file.split(" - ")
     new_song = self.new(song)
     new_artist = Artist.find_or_create_by_name(name)
     new_song.artist = new_artist
