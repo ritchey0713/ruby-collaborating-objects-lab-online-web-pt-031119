56e8339739e334559252687650513c0dc0888792
David Ritchey <ritchey0713@gmail.com>
2019-04-12 22:32:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,6 +10,7 @@ class Song
     artist, song = file.split(" - ")
     new_song = self.new(song)
     new_artist = Artist.find_or_create_by_name(name)
+    new_artist.add_song(new_song)
     new_song.artist = new_artist
     new_song
   end
